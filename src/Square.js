bda7e8aa2048283aee81a8cbfff1ea8a1ea7915b
andypb <andy.birchall@gmail.com>
2021-11-24 11:14:59 +0000

    working version with components split out from index.js

---
+++ src/Square.js
@@ -0,0 +1,8 @@
+export default function Square({value = '', onClick = (f) => f}) {
+
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    );
+}
