6251752de6dff7448a6db1789d5929c6240cd73c
andypb <andy.birchall@gmail.com>
2021-11-25 15:44:32 +0000

    navigate history and resetting game etc.

--- src/Game.js
+++ src/Game.js
@@ -8,24 +8,51 @@ export default function Game() {
 
     const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
     const [isXNext, setIsXNext] = useState(true);
+    const [selectedMove, setSelectedMove] = useState(0);
 
-    const current = history[history.length - 1];
-    const squares = current.squares;
+    const current = history[selectedMove];
+    let squares = current.squares;
     const winner = calculateWinner(squares);
 
     let status;
     if (winner) {
         status = 'Winner: ' + winner
     } else {
-        status = 'Next player: ' + (isXNext ? 'X' : 'O');
+        if (!squares.includes(null)) { status = 'Game over, no-one won' }
+        else { status = 'Next player: ' + (isXNext ? 'X' : 'O'); }
     }
+
+    const moves = history.map((historyStep, moveNum) => {
+       const desc = moveNum > 0 ? `Go to move # ${moveNum}` : 'Go to game start';
+       return (
+        <li key={moveNum}>
+           <button onClick={() => jumpToMove(moveNum)}>{desc}</button>
+        </li>
+       );
+    });
 
     function handleSquareClick(i) {
         if (winner || squares[i]) { return; }
         const squaresCopy = squares.slice();
-        squaresCopy[i] = isXNext ? 'X' : 'O'
-        setHistory(history.concat([{squares: squaresCopy}]));
+        squaresCopy[i] = isXNext ? 'X' : 'O';
+        const historyCopy = getUpdatedHistory(selectedMove);
+        setSelectedMove(historyCopy.length);
+        setHistory(historyCopy.concat([{squares: squaresCopy}]));
         setIsXNext(!isXNext);
+    }
+
+    /**
+     * if moveNum < (history.length - 1) then history will be truncated to the moveNum+1 size,
+     * so that the new move (square has been clicked) resets the game from that position
+     * */
+    function getUpdatedHistory(moveNum) {
+        return history.slice(0, moveNum + 1);
+    }
+
+    function jumpToMove(moveNum) {
+        setSelectedMove(moveNum);
+        setIsXNext(moveNum % 2 === 0);
+        if (moveNum === 0) { setHistory(getUpdatedHistory(moveNum)); } // reset history
     }
 
     return (
@@ -35,7 +62,7 @@ export default function Game() {
             </div>
             <div className="game-info">
                 <div>{status}</div>
-                <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
